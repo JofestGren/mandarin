f0b2dc8242d603b9d046a9e4332f57aa086a3a7b
JofestGren <jofest.g.70@gmail.com>
2018-10-20 15:24:54 +0300

    finish

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -51,156 +51,82 @@ window.onload = () => {
     navSearch.classList.add('active');
   })
 
-//     const popupFeedback = document.getElementById('popupFeedback');
-//     const feedbackBtn = document.querySelectorAll('.feedback-btn');
-//     const popupFeedbackClose = document.querySelector('.popup-feedback-close');
+  const popupMenu = document.getElementById('popupMenu');
+  const popupMenuClose = document.querySelector('.popup-menu-close');
+  const navSandwich = document.querySelector('.nav__sandwich');
 
-//     feedbackBtn.forEach(element => {
-//         element.addEventListener('click', event => {
-//             popupFeedback.style.display = 'flex';
-//         })
-//     })
-    
-//     popupFeedbackClose.addEventListener('click', event => {
-//         popupFeedback.style.display = 'none';
-//     })
+  navSandwich.addEventListener('click', event => {
+    navSandwich.classList.add('active');
+    popupMenu.style.display = 'flex';
+  })
 
-//     const actionsConsultationBtn = document.querySelector('.actions-consultation__btn');
+  popupMenuClose.addEventListener('click', event => {
+    popupMenu.style.display = 'none';
+    navSandwich.classList.remove('active');
+  })
 
-//     if (window.innerWidth < 1024) {
-//         actionsConsultationBtn.addEventListener('click', event => {
-//             event.preventDefault();
-//             popupFeedback.style.display = 'flex';
-//         })
-//     }
+  const popupMenuLanguage = document.querySelector('.popup-menu-language');
+  popupMenuLanguage.addEventListener('click', event => {
+    let target = event.target;
 
-//     const sizeRoof = document.getElementById('sizeRoof');
-//     const roofLine = document.getElementById('roofLine');
+    while (target != popupMenuLanguage) {
+      if (target.tagName == 'A') {
+        const childrenNavLanguage = Array.from(popupMenuLanguage.children);
+        childrenNavLanguage.forEach(element => {
+          element != target ? element.classList.remove('active') :
+          target.classList.add('active')
+        })
+        return;
+      }
+      target = target.parentNode;
+    }
+  })
 
-//     sizeRoof.addEventListener('mouseenter', event => {
-//         roofLine.style.display = 'block';
-//     })
+  const popupMenuLink = document.querySelectorAll('.popup-menu__link');
 
-//     sizeRoof.addEventListener('mouseleave', event => {
-//         roofLine.style.display = 'none';
-//     })
+  popupMenuLink.forEach( element => {
+    element.addEventListener('click', event => {
+      popupMenu.style.display = 'none';
+      navSandwich.classList.remove('active');
+    })
+  })
 
-//     const popupMenu = document.getElementById('popupMenu');
-//     const popupMenuClose = document.querySelector('.popup-menu-close');
-//     const popupMenuList = document.querySelector('.popup-menu__list');
+  const menuMobileBtn = document.querySelector('.menu-mobile__btn');
+  const menuMobileHeader = document.querySelector('.menu-mobile__header');
+  const menuMobileCategory = document.querySelector(
+    '.menu-mobile__category'
+  )
 
-//     const homeSandwich = document.querySelector('.home__sandwich');
-//     homeSandwich.addEventListener('click', event => {
-//         homeSandwich.classList.toggle('active');
-//         popupMenu.style.display = 'flex'
-//     })
+  menuMobileBtn.addEventListener('click', evnet => {
+    menuMobileBtn.classList.toggle('active');
+    menuMobileHeader.classList.toggle('active');
+    menuMobileCategory.classList.toggle('active');
+  })
 
-//     window.addEventListener('click', event => {
-//         if (event.target == popupFeedback) {
-//           popupFeedback.style.display = 'none';
-//         } else if (event.target == popupMenu) {
-//             popupMenu.style.display = 'none';
-//             homeSandwich.classList.remove('active');
-//         }
-//     });
-    
-//     popupMenuClose.addEventListener('click', event => {
-//         popupMenu.style.display = 'none';
-//         homeSandwich.classList.remove('active');
-//     })
+  const addAccordion = arrayChildren => {
+    arrayChildren.forEach(element => {
+      element.addEventListener('click', event => {
+        const title = element.children[0];
+        title.classList.toggle('active');
+        const panel = title.nextElementSibling;
+        const count = Array.from(panel.children).length;
 
-//     popupMenuList.addEventListener('click', event => {
-//         let target = event.target;
+        panel.style.maxHeight ? panel.style.maxHeight = null :
+        panel.style.maxHeight = `${40*count}px`;
+        element.parentElement.style.maxHeight = 'none';
+      })
+    })
+  }
 
-//         while (target != popupMenuList) {
-//             if (target.tagName == 'A') {
-//                 popupMenu.style.display = 'none';
-//                 homeSandwich.classList.remove('active');
-//                 return;
-//             }
-//             target = target.parentNode;
-//         }
-//     })
+  const menuMobilePanel = document.querySelectorAll('.menu-mobile__name');
 
-    
-
-//     // Custom select don't touch the dangerous zone !!!
-//     var i, j, selElmnt, a, b, c;
-//     /*look for any elements with the class "custom-select":*/
-//     var select = document.getElementsByClassName('calculation-content__select');
-
-//     var selectorCustom = function (x) {
-//         for (i = 0; i < x.length; i++) {
-//             selElmnt = x[i].getElementsByTagName("select")[0];
-//             /*for each element, create a new DIV that will act as the selected item:*/
-//             a = document.createElement("DIV");
-//             a.setAttribute("class", "select-selected");
-//             a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-//             x[i].appendChild(a);
-//             /*for each element, create a new DIV that will contain the option list:*/
-//             b = document.createElement("DIV");
-//             b.setAttribute("class", "select-items select-hide");
-//             for (j = 0; j < selElmnt.length; j++) {
-//                 /*for each option in the original select element,
-//                 create a new DIV that will act as an option item:*/
-//                 c = document.createElement("DIV");
-//                 c.innerHTML = selElmnt.options[j].innerHTML;
-//                 c.addEventListener("click", function(e) {
-//                     /*when an item is clicked, update the original select box,
-//                     and the selected item:*/
-//                     var y, i, k, s, h;
-//                     s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-//                     h = this.parentNode.previousSibling;
-//                     for (i = 0; i < s.length; i++) {
-//                     if (s.options[i].innerHTML == this.innerHTML) {
-//                         s.selectedIndex = i;
-//                         h.innerHTML = this.innerHTML;
-//                         y = this.parentNode.getElementsByClassName("same-as-selected");
-//                         for (k = 0; k < y.length; k++) {
-//                         y[k].removeAttribute("class");
-//                         }
-//                         this.setAttribute("class", "same-as-selected");
-//                         break;
-//                     }
-//                     }
-//                     h.click();
-//                 });
-//                 b.appendChild(c);
-//             }
-//             x[i].appendChild(b);
-//             a.addEventListener("click", function(e) {
-//                 /*when the select box is clicked, close any other select boxes,
-//                 and open/close the current select box:*/
-//                 e.stopPropagation();
-//                 closeAllSelect(this);
-//                 this.nextSibling.classList.toggle("select-hide");
-//                 this.classList.toggle("select-arrow-active");
-//             });
-//         }
-//         function closeAllSelect(elmnt) {
-//         /*a function that will close all select boxes in the document,
-//         except the current select box:*/
-//         var x, y, i, arrNo = [];
-//         x = document.getElementsByClassName("select-items");
-//         y = document.getElementsByClassName("select-selected");
-//         for (i = 0; i < y.length; i++) {
-//             if (elmnt == y[i]) {
-//             arrNo.push(i)
-//             } else {
-//             y[i].classList.remove("select-arrow-active");
-//             }
-//         }
-//         for (i = 0; i < x.length; i++) {
-//             if (arrNo.indexOf(i)) {
-//             x[i].classList.add("select-hide");
-//             }
-//         }
-//         }
-//         /*if the user clicks anywhere outside the select box,
-//         then close all select boxes:*/
-//         document.addEventListener("click", closeAllSelect);
-//     }
-
-//     selectorCustom(select)
-    
+  menuMobilePanel.forEach(element => {
+    element.addEventListener('click', event => {
+      element.classList.toggle('active');
+      const panel = element.nextElementSibling;
+      panel.style.maxHeight ? panel.style.maxHeight = null :
+      panel.style.maxHeight = panel.scrollHeight + "px"
+      addAccordion(Array.from(panel.children))
+    })
+  })
 }
--- src/static/js/plugin.js
+++ src/static/js/plugin.js
@@ -1,49 +1,21 @@
 $(document).ready(function () {
-    // $(function() {
-    //     $('.scroll-link').click(function() {
-    //       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    //         var target = $(this.hash);
-    //         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    //         if (target.length) {
-    //           $('html, body').animate({
-    //             scrollTop: target.offset().top
-    //           }, 1000);
-    //           return false;
-    //         }
-    //       }
-    //     });
-    // });
-
-    // $(function () {
-    //     $("#popupFeedbackPhone").mask("+38 (999) 999-99-99");
-    //     $("#actionsPhone").mask("+38 (999) 999-99-99");
-    // })
-
     $('.js-home-slider').slick({
       arrows: true,
       dots: true,
       infinite: true,
-      // slidesToShow: 1,
-      // slidesToScroll: 1,
       prevArrow: '.home-slider__prev',
       nextArrow: '.home-slider__next',
       customPaging: function (slider, i) {
         return '<span class="home-slider__dot"></span>';
       },
-      // responsive: [
-      //   {
-      //     breakpoint: 1365,
-      //     settings: {
-      //       slidesToShow: 3
-      //     }
-      //   },
-      //   {
-      //     breakpoint: 765,
-      //     settings: {
-      //       slidesToShow: 2
-      //     }
-      //   },
-      // ]
+      responsive: [
+        {
+          breakpoint: 765,
+          settings: {
+            arrows: false
+          }
+        },
+      ]
     })
 
     $('.js-home-product-slider').slick({
